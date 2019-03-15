dc86a130b4bf105af30cd6ef40e9d295ee1fbf21
Josef Renner <josef.renner@gmail.com>
2019-03-15 08:58:03 +0100

    finalize slider

--- web/assets/js/initializeSlider.js
+++ web/assets/js/initializeSlider.js
@@ -2,6 +2,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var elem = document.querySelector('.carousel');
   var flkty = new Flickity( elem, {
     // options
-    wrapAround: true
+    wrapAround: true,
+    pageDots: false,
+    prevNextButtons: false,
+    draggable: false
   });
+
+  var images = document.querySelectorAll('.carousel-cell');
+  for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click' || 'touchend', function() {
+      flkty.next( true, true );
+    });
+  }
 });
+
+
