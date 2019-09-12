ce8ab5ee01ca40b10b86ecbb43171eaa34aa16c2
Josef Renner <josef.renner@gmail.com>
2019-09-12 20:52:40 +0200

    add portfolio upload

--- config/general.php
+++ config/general.php
@@ -26,6 +26,8 @@ return [
         // Whether to save the project config out to config/project.yaml
         // (see https://docs.craftcms.com/v3/project-config.html)
         'useProjectConfigFile' => true,
+
+        'maxUploadFileSize' => 60777216
     ],
 
     // Dev environment settings
